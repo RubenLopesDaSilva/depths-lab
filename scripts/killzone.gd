8720ee72139934d43e6b9d08606a62764022a5d3
BurriSimon <simon.burri@ceff.ch>
2025-11-18 21:31:52 +0100

    Update 21:31 18.11.2025 Correction

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,11 @@
 extends Area2D
 @onready var timer: Timer = $Timer
 
-
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	body.take_damage();
-	print(body.health)
 	if(body.health == 0):
 		body.Death();
-		Engine.time_scale = 0.5
+		Engine.time_scale = 0.2
 		timer.start()
 
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,42 +4,32 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -550.0
 var next_animation = "idle"
-var is_dead = false;
-var i_frames = false;
+var dying = false;
+var taking_damage = false;
 var health = 100;
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var timer: Timer = $Timer
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
 func _physics_process(delta: float) -> void:
-	if(is_dead):
-		return;
+	var direction := Input.get_axis("move_left", "move_right")
+	if not dying:
+		if (Input.is_action_just_pressed("attack")):
+			next_animation = "Attack"
+			Attack()
+			await animated_sprite.animation_finished
+			next_animation = "Idle"
+			
+		if animated_sprite.animation == "GetDamage":
+			direction = 0
+			await animated_sprite.animation_finished
+			next_animation = "Idle"
 		
-	var direction := Input.get_axis("ui_left", "ui_right")
-	
-	if (Input.is_action_just_pressed("Attack")):
-		next_animation = "Attack"
-		Attack()
-		await animated_sprite.animation_finished
-		next_animation = "Idle"
-		
-	if animated_sprite.animation == "GetDamage":
-		direction = 0
-		await animated_sprite.animation_finished
-		next_animation = "Idle"
-		
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			next_animation = "Jump"
+			velocity.y = JUMP_VELOCITY
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		next_animation = "Jump"
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -60,19 +50,21 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func Death() -> void:
-	is_dead = true;
+	dying = true;
 	animated_sprite.play("Death");
+	await  animated_sprite.animation_finished
+	dying = false
 
 func take_damage() -> void:
-	if not i_frames:
+	if not taking_damage:
 		next_animation = "GetDamage"
 		animated_sprite.play("GetDamage")
 		health -= 20;
-		i_frames = true
+		taking_damage = true
 		timer.start()
 
 func Attack()-> void:
 	animated_sprite.play("FirstAttack")
 
 func _on_timer_timeout() -> void:
-	i_frames = false;
+	taking_damage = false;
