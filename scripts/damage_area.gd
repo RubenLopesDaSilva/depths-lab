80b1fd9f130aa9ee0ed1ec2b075110525d9e8ceb
BurriSimon <simon.burri@ceff.ch>
2026-03-19 11:25:23 +0100

    Update 11:25 19.03.2026

---
+++ scripts/damage_area.gd
@@ -0,0 +1,13 @@
+class_name AttackArea extends Area2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	area_entered.connect(_on_body_entered)
+	monitorable = false
+	monitoring = true
+	pass
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body is DamageArea:
+		pass
+	pass
