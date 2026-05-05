0d6d76dfcdaebef8d9cd6f4c4e1279eb4320eaae
BurriSimon <simon.burri@ceff.ch>
2026-05-05 21:55:04 +0200

    Update 21:54 05.05.2026

--- scripts/new_mossy.gd
+++ scripts/new_mossy.gd
@@ -9,6 +9,7 @@ var direction = 1;
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_bottom: RayCast2D = $RayCastBottom
 
 func _physics_process(delta: float) -> void:
 	update_direction()
@@ -17,7 +18,7 @@ func _physics_process(delta: float) -> void:
 	apply_movement(delta)
 
 func update_direction() -> void:
-	if ray_cast_right.is_colliding():
+	if ray_cast_right.is_colliding() || not ray_cast_bottom.is_colliding():
 		direction = direction * -1;
 	#if ray_cast_left.is_colliding():
 		#direction = 1;
