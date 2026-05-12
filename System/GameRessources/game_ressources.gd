76bc2ff4f390ae0375899479e5e8483a8312f85e
RubenLopesDaSilva <ruben.lopesdasilva@ceff.ch>
2026-05-12 14:01:39 +0200

    Suite boss

---
+++ System/GameRessources/game_ressources.gd
@@ -0,0 +1,55 @@
+class_name GameResource
+extends Node
+
+signal depleated
+signal replenished
+signal max_changed(new_max: int)
+signal current_changed(new_current: int)
+var idle_finished_count = 0;
+
+
+@export var max_amount := 10 : set = set_max_amount
+@export var current_amount := 0 : set = set_current_amount
+
+
+func _ready() -> void:
+	max_changed.emit(max_amount)
+	current_changed.emit(current_amount)
+
+
+func set_max_amount(new_max_amount: int) -> void:
+	max_amount = new_max_amount
+	max_changed.emit(max_amount)
+	if current_amount > max_amount:
+		current_amount = new_max_amount
+
+
+func set_current_amount(new_amount: int) -> void:
+	current_amount = clampi(new_amount, 0, max_amount)
+	current_changed.emit(current_amount)
+	if new_amount < 1:
+		depleated.emit()
+	elif new_amount >= max_amount:
+		replenished.emit()
+
+
+func increase(amount: int) -> void:
+	current_amount += amount
+
+
+func decrease(amount: int) -> void:
+	current_amount -= amount
+
+
+func deplete() -> void:
+	current_amount = 0
+
+
+func replenish() -> void:
+	current_amount = max_amount
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == 'Idle':
+		idle_finished_count += 1
+		print("Idle animation finished: ", idle_finished_count, " times")
