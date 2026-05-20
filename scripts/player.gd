23f3754d3f75612b4dee8aa691ff59e96c40d876
BurriSimon <simon.burri@ceff.ch>
2026-05-20 23:19:19 +0200

    Update 23:19 20.05.2026

--- scripts/player.gd
+++ scripts/player.gd
@@ -151,7 +151,7 @@ func play_land() -> void:
 	
 func play_attack() -> void:
 	if attack_state == AttackState.FIRST:
-		await Utils.delay(0.5)
+		await Utils.delay(0.2)
 		close = true
 		if attack_state == AttackState.FIRST:
 			animation_player.play("FirstAttack")
