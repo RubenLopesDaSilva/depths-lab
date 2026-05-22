9601de0af28511bd8aad0f7847fb4ceb690a17df
BurriSimon <simon.burri@ceff.ch>
2026-05-22 15:31:05 +0200

    Update 15:30 22.05.2026

--- 00_global/save_manager.gd
+++ 00_global/save_manager.gd
@@ -1,10 +1,12 @@
-# Save manager script
 extends Node
 
-var current_slot : int = 0;
-var save_data : Dictionary;
-var discovered_areas : Array = [];
-var persistent_data : Dictionary = {};
+const SAVE_PATH : String = "user://gamedata.json";
+
+#var current_slot : int = 0;
+var save_data : Dictionary = {};
+var check_points : Array[String] = [];
+#var discovered_areas : Array = [];
+#var persistent_data : Dictionary = {};
 
 
 
@@ -20,30 +22,27 @@ func _unhandled_key_input(event: InputEvent) -> void:
 			load_game();
 		elif  event.keycode == KEY_F2:
 			save_game();
-	pass
 
 func save_game() -> void:
 	print("save game");
-	pass
 
 func load_game() -> void:
 	print("load game");
-	pass
 
 func create_new_game_save() -> void:
-	var new_game_scene : String ="uid://cdtwgxyy3f0f4";
-	discovered_areas.append(new_game_scene);
+	#var new_game_scene : String ="uid://cdtwgxyy3f0f4";
+	#discovered_areas.append(new_game_scene);
 	save_data = {
-		"scene_path": new_game_scene,
+		#"scene_path": new_game_scene,
 		"x": -205,
 		"y": -1,
-		"hp": 200,
-		"max_hp": 200,
-		"dash": false,
-		"double_jump": false,
-		"discovered_areas": discovered_areas,
-		"persistent_data": persistent_data
+		"collectable": 0
+		#"max_hp": 200,
+		#"dash": false,
+		#"double_jump": false,
+		#"discovered_areas": discovered_areas,
+		#"persistent_data": persistent_data
 	}
+	
 	var save_file = FileAccess.open("user://save.sav",FileAccess.WRITE);
 	save_file.store_line( JSON.stringify(save_data));
-	pass
