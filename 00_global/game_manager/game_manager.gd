2dd6eab1c1f4aaa75dbd82331c7f27652f275799
BurriSimon <simon.burri@ceff.ch>
2026-05-25 09:55:26 +0200

    Update 09:55 25.05.2026

--- 00_global/game_manager/game_manager.gd
+++ 00_global/game_manager/game_manager.gd
@@ -8,10 +8,11 @@ var _map_is_ready : bool = false;
 var _death_timer : Timer
 
 func _ready() -> void:
+	pass
 	_death_timer = Timer.new()
 	_death_timer.one_shot = true
-	_death_timer.wait_time = 1
-	_death_timer.timeout.connect(restart)
+	_death_timer.wait_time = 2
+	_death_timer.timeout.connect(_end_death)
 	add_child(_death_timer)
 	
 func restart() -> void:
@@ -27,7 +28,10 @@ func reset() -> void:
 func dying() -> void:
 	AudioManager.change_state(AudioManager.MusicState.DEATH);
 	Engine.time_scale = 0.5
-	#_death_timer.start()
+	_death_timer.start()
+	
+func _end_death() -> void :
+	MenuCanva._show();
 	
 func emit_map_ready() -> void:
 	_map_is_ready = true;
